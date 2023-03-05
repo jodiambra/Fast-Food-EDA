3961617b534a3259959b7b9c00e7a1071cfa13ab
jodiambra <jodiambra@gmail.com>
2023-03-05 17:10:59 -0500

    sales page

--- pages/1_Sales.py
+++ pages/1_Sales.py
@@ -42,6 +42,7 @@ df.loc[np.isinf(df['company_store_ratio']), 'company_store_ratio'] = 0
 
 #----------------------------------------# 
 
+# distribution of us sales
 st.subheader('US Sales')
 
 st.write(px.histogram(df.us_sales_millions, title='Distribution of US Sales', template='ggplot2',
@@ -55,9 +56,46 @@ with st.expander('Details'):
 st.title('')
 st.title('')
 #--------------------------------------------#
+# distribution of unit sales 
 st.subheader('Unit Sales')
 st.write(px.histogram(df.average_unit_sales_thousands, title='Distribution of Average Unit Sales', template='ggplot2',
                       labels={'value': 'Average Unit Sales'}, color_discrete_sequence=['green'], height=700, width=900))
 with st.expander('Details'):
-    st.write('We see the distribution of total sales is right skewed, with most chains making less than 4.9 billion dollars.',
-             'One chain in particular makes more than 45 billion dollars in sales in the US.')
+    st.write('The distribution of unit sales seams to be normal, with the most values between 1.0 to 1.5 million dollars in sales.')
+
+st.title('')
+st.title('')
+#-----------------------------------------#
+# top sales 
+st.subheader('Top Sales')
+number = st.slider('Selection', min_value=10, max_value=50, step=10)
+st.write(px.bar(df.sort_values(by='us_sales_millions', ascending=False).head(number), x='name', y='us_sales_millions', title='Top ' +str(number)+ ' US Sales', 
+       labels={'us_sales_millions': 'Sales (millions)'}, height=700, width=900, color='name'))
+with st.expander('Details'):
+    st.write('McDonald\'s far exceeds the other fast food chains in terms of sales. Starbucks is second, with roughly half the total sales of',
+             'Mcdonald\'s. Chick-Fil-A, Taco Bell and Wendy\'s follows close behind Starbucks')
+
+st.title('')
+st.title('')
+#---------------------------------------------# 
+
+# top unit sales 
+st.subheader('Top Sales')
+# uses slider from first graph
+st.write(px.bar(df.sort_values(by='average_unit_sales_thousands', ascending=False).head(number), x='name', y='average_unit_sales_thousands', title='Top ' +str(number)+ ' US Unit Sales', 
+       labels={'value': 'Unit Sales (thousands)'}, height=700, width=900, color='name'))
+with st.expander('Details'):
+    st.write('The most profitable units are found with Chick-Fil-A, Raising Cane\'s, Krispy Kreme, and Shake Shack. Surprisingly,',
+             'McDonald\'s comes in 6th place.')
+
+st.title('')
+st.title('')
+#------------------------------------------# 
+
+# market share
+st.subheader('Market Share')
+# uses slider from first graph
+st.write(px.pie(df.sort_values(by='share_of_sales', ascending=False).head(number), names='name', values='share_of_sales', title='Top ' +str(number)+ ' Market Share', 
+    height=700, width=900, color='name'))
+with st.expander('Details'):
+    st.write('McDonald\'s dominates the market share of the fast food market, followed by Starbucks, and then Chick-Fil-A.')
